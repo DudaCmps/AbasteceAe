7661563488016a58e84e800a813193173f5e4d99
DudaCmps <santcamposeduarda@gmail.com>
2026-02-23 16:47:18 -0300

    feat: setup tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+    {{ config('app.name') }}
+  </title>
+
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-pink-400">
+    {{ config('app.name') }}
+  </h1>
+</body>
+</html>
