f783dfe999dd4a2433a71c2f838f91f47c575437
DudaCmps <santcamposeduarda@gmail.com>
2026-03-06 18:00:06 -0300

    feat: add address management modal

--- resources/views/components/admin/layout_admin.blade.php
+++ resources/views/components/admin/layout_admin.blade.php
@@ -11,6 +11,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=home" />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
   <!-- Nucleo Icons -->
--- resources/views/dashboard/tables.blade.php
+++ resources/views/dashboard/tables.blade.php
@@ -19,6 +19,7 @@
                   <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
                   <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nascimento</th>
                   <th class="text-secondary opacity-7"></th>
+
                 </tr>
                 </thead>
                 <tbody>
@@ -51,8 +52,11 @@
                       <span class="text-secondary text-xs font-weight-bold">{{ $user->date_of_birth}}</span>
                     </td>
                     <td class="align-middle">
-                      <a class="pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser({{$user->id}})">
-                        Edit
+                      <a class="cursor-pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser({{$user->id}})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#737373" viewBox="0 0 256 256"><path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.68,147.31,64l24-24L216,84.68Z"></path></svg>
+                      </a>
+                      <a class="cursor-pointer text-secondary font-weight-bold text-xs ms-2 " data-bs-toggle="modal" data-bs-target="#modal-address-user" onclick="addressUser({{$user->id}})">
+                        <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#737373" viewBox="0 0 256 256"><path d="M219.31,108.68l-80-80a16,16,0,0,0-22.62,0l-80,80A15.87,15.87,0,0,0,32,120v96a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V160h32v56a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V120A15.87,15.87,0,0,0,219.31,108.68ZM208,208H160V152a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8v56H48V120l80-80,80,80Z"></path></svg>
                       </a>
                     </td>
                   </tr>
@@ -77,9 +81,9 @@
       <div class="modal-content">
         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
           <div class="bg-gradient-dark shadow-dark border-radius-lg  d-flex  justify-content-between">
-{{--            pt-4 pb-3--}}
+            {{--            pt-4 pb-3--}}
             <img src="../assets/img/team-1.jpg" class="avatar-radius ms-3" alt="user1">
-{{--            <button type="button" class="btn-close pe-4 align-self-center" data-bs-dismiss="modal"></button>--}}
+            {{--            <button type="button" class="btn-close pe-4 align-self-center" data-bs-dismiss="modal"></button>--}}
           </div>
         </div>
 
@@ -159,6 +163,139 @@
           <div>
             <button class="btn btn-outline-danger">Excluir Colaborador</button>
           </div>
+          <div class="d-flex gap-2">
+            <button class=" btn btn-ligt-adapt text-dark border border-dark" data-bs-dismiss="modal">Cancelar</button>
+            <button class="btn btn-dark" data-bs-dismiss="modal">Salvar Mudanças</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+{{--  MODAL DO ENDEREÇO--}}
+  <div class="modal fade" id="modal-address-user" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+      <div class="modal-content">
+        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+          <div class="bg-gradient-dark shadow-dark border-radius-lg  d-flex  justify-content-between">
+            {{--            pt-4 pb-3--}}
+            <img src="../assets/img/team-1.jpg" class="avatar-radius ms-3" alt="user1">
+            {{--            <button type="button" class="btn-close pe-4 align-self-center" data-bs-dismiss="modal"></button>--}}
+          </div>
+        </div>
+
+        <div class="modal-header ">
+          <div class="d-flex flex-column align-items-start ms-3 mt-3">
+            <h4 class="text-lg mb-0" id="name_text">Maria</h4>
+            <p class="text-xs text-secondary mb-0" id="email_text">
+              maria@gmail.com
+            </p>
+          </div>
+        </div>
+
+
+{{--            INICIO--}}
+            <div class="table-responsive p-0">
+              <table class="table align-items-center mb-0">
+                <thead>
+                <tr>
+                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">CEP</th>
+                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">UF</th>
+                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Cidade</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Bairro</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Logradouro</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Número</th>
+                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Complemento</th>
+                  <th class="text-secondary opacity-7"></th>
+
+                </tr>
+                </thead>
+                <tbody>
+
+                <tr>
+                  <td>
+                    <div class="d-flex px-2 py-1">
+
+                      <div class="d-flex flex-column justify-content-center">
+                        <p class="text-xs font-weight-bold mb-0">13868-002</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">SP</p>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">Aguaí</p>
+                  </td>
+                  <td class="align-middle text-center text-sm">
+                    <p class="text-xs font-weight-bold mb-0">Vila Nova</p>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">Eleodoro Matheus de Morães</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">Bloco A - 48</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">AP</span>
+                  </td>
+                  <td class="align-middle">
+                    <a class="cursor-pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser(2)">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#737373" viewBox="0 0 256 256"><path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.68,147.31,64l24-24L216,84.68Z"></path></svg>
+                    </a>
+                    <a class="cursor-pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser(1)">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#CE0000" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg>
+                    </a>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td>
+                    <div class="d-flex px-2 py-1">
+
+                      <div class="d-flex flex-column justify-content-center">
+                        <p class="text-xs font-weight-bold mb-0">13868-002</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">MG</p>
+                  </td>
+                  <td>
+                    <p class="text-xs font-weight-bold mb-0">Montes Claros</p>
+                  </td>
+                  <td class="align-middle text-center text-sm">
+                    <p class="text-xs font-weight-bold mb-0">Vila Regina</p>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">José Divino Lopes</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">45</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">Casa</span>
+                  </td>
+                  <td class="align-middle">
+                    <a class="cursor-pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser(2)">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#737373" viewBox="0 0 256 256"><path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.68,147.31,64l24-24L216,84.68Z"></path></svg>
+                    </a>
+                    <a class="cursor-pointer text-secondary font-weight-bold text-xs" data-bs-toggle="modal" data-bs-target="#modal-edit-user" onclick="updateUser(1)">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#CE0000" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg>
+                    </a>
+                  </td>
+                </tr>
+
+
+                </tbody>
+              </table>
+            </div>
+{{--          FIM--}}
+
+        <div class="modal-footer justify-content-end">
+
           <div class="d-flex gap-2">
             <button class=" btn btn-ligt-adapt text-dark border border-dark" data-bs-dismiss="modal">Cancelar</button>
             <button class="btn btn-dark" data-bs-dismiss="modal">Salvar Mudanças</button>
